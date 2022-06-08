61d31cb591ff75536d205e7d7ab03ec41f3b6803
yohannesnan <yohannessetegn18@gmail.com>
2022-06-08 16:13:51 +0300

    modified:   script.js
    completely changed approach
    
    i wish i could have done it here but i didn't
    i have learned a big lesson to start building
    from less complicated concept.

--- script.js
+++ script.js
@@ -1,58 +1,109 @@
-function computerPlay(){
-    let pointer =  Math.floor((Math.random())*3);
 
-    if(pointer === 0) 
-        {return('Rock');}
-    else if(pointer === 1) 
-        {return('Paper');}
-    else
-        {return('Scissors');}
+
+function random(num){
+    return (Math.floor(Math.random() * num));
 }
 
-let x = prompt("choose Rock, Paper, Scissors", "");
 
 
-let playerScore=0, computerScore=0;
-let computerSelection = computerPlay();
+let string = "";
+let c1Score = 0, humanScore = 0, c2='rock';
 
-
-function round(playerSelection, computerSelection){
-    if(playerSelection === computerSelection)
+function retrieve(){
+    let tempoHolder = (prompt("rock,paper,scissor", ""));
+    c2 = tempoHolder.toLowerCase();
+    if(c2 === 'rock' || 'paper' || 'scissor')
     {
-        console.log("Draw! " + playerSelection + " can't beat " + computerSelection);
-        console.log("computer "+ computerScore + ":" + playerScore + " player")
-
-    }
-    else if((playerSelection === "Paper" && computerSelection === "Rock")||
-    (playerSelection === "Rock" && computerSelection === "Scissors")||
-    (playerSelection === "Scissors" && computerSelection === "Paper"))
-    {
-        console.log("You Win! " + playerSelection + " beats " + computerSelection);
-        ++playerScore;
-        console.log("computer "+ computerScore + ":" + playerScore + " player")
-   
+        return c2;
     }
     else{
-        console.log("You Lost! " + computerSelection  + " beats " + playerSelection);
-        ++computerScore;
-        console.log("computer "+ computerScore + ":" + playerScore + " player")
-        
+        correctInput();
     }
 }
 
-round(x, computerSelection);
+function correctInput(){
+    let tempo2Holder = (prompt("your input wasn't correct! please enter correctly", ""));
+    c2 = tempo2Holder.toLowerCase();
 
-let leader;
-if(playerScore >= computerScore){
-    leader = playerScore;
-}
-else{
-    leader = computerScore;
+    if(c2 === 'rock' || 'paper' || 'scissor')
+    {
+        return c2;
+    }
+    else{
+        correctInput();
+    }
 }
 
-for(leader; leader < 5; leader++)
+function turnToString(num=0)
 {
-    let y = prompt("choose Rock, Paper, Scissors", "");
-    
-    
+    if(num === 0)
+    {
+        return ("rock");
+    }
+    else if(num === 1)
+    {
+        return ("paper");
+    }
+    else
+    {
+        return("scissor");
+    }
 }
+
+
+function round(com , hum){
+    if((com == 'rock' && hum == 'scissor')||
+    (com == 'scissor' && hum == 'paper')||
+    (com == 'paper' && hum == 'rock'))
+    {
+        // prompt (com + " beats " + hum);
+        console.log ("computer/" + com + " beats " + hum + "/human" );
+        ++c1Score;
+    }
+    else if(com == hum)
+    {
+        // prompt (com + " can't beats " + hum);
+        console.log ("computer/" + com + " can't beat " + hum + "/human" );
+    }
+    else
+    {
+        // prompt (hum + " beats " + com);
+        console.log ("human/" + hum + " beats " + com + "/computer" );
+        ++humanScore;
+    }
+}
+
+
+function letTheGamesBiggin(){
+    for(let i=0; i<5; i++)
+    {
+        retrieve();// assign c2(humanValue) to the typed option
+
+        let c1;
+        c1 = random(3);// draws random number from 0 to 2(including)
+        turnToString(c1);// returns rock, paper , scissor from random number
+        round(turnToString(c1), c2); // compares the results
+
+        if (c1Score <= humanScore)
+        {
+            i = humanScore - 1;
+        }
+
+        else{i = c1Score;}
+    }   
+}
+
+letTheGamesBiggin();
+
+let winner;
+if (humanScore > c1Score)
+{
+    winner = "human wins!";
+}
+else
+{
+    winner = "computer wins!";   
+}
+
+console.log("\n"+ winner + "\t" + "\t" + "computer " + c1Score + ":" + humanScore + " human");
+  
